096613c1e15f48165be6b1bf158342669d7d5127
Tanner Linsley <tannerlinsley@gmail.com>
2016-02-27 22:42:32 -0600

    Parse module beginnings

---
+++ src/parse.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import _ from './lodash'
+
+import operators from './operators'
+
+module.exports = function(service) {
+  return function parse(query) {
+    var levels = []
+
+
+  }
+}
+
+function go(query, levels){
+  if(_.isArray(query)){
+
+  }
+}
